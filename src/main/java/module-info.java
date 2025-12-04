609877d7c7bae84411f21d215bbace5148e051d8
Irfane WABI <irfane.wabi@edu.ece.fr>
2025-12-05 00:43:32 +0100

    partis de Irfane niveau 1 Dashbord admin

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,13 @@
 module org.example.snake {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires java.sql;
+    requires java.desktop; // C'est cette ligne qui corrige ton erreur rouge !
 
     opens org.example.snake to javafx.fxml;
     exports org.example.snake;
+
+    // Autorise JavaFX à lire tes contrôleurs et tes modèles
+    opens org.example.snake.controller to javafx.fxml;
+    opens org.example.snake.model to javafx.base;
 }
--- src/main/java/org/example/snake/HelloApplication.java
+++ src/main/java/org/example/snake/HelloApplication.java
@@ -10,10 +10,15 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        // Charge l'interface admin.fxml
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("admin.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Snake Admin & Jeu");
         stage.setScene(scene);
         stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
     }
 }
